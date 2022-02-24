1725956d44f7f6c98709ff66ace06458f2b34074
Ariel Molho <arielmolho@globaltask.net>
2022-02-24 19:17:07 -0300

    Home and Readme adjustments

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HomeCarousel from '../../components/HomeCarousel/HomeCarousel';
 import CardHome from '../../components/CardHome/CardHome';
 import { Container, Image, Row, Carousel } from 'react-bootstrap';
@@ -14,17 +14,6 @@ export default function Home() {
   const InfoDry = "./img/Tela-Dry-Info.png";
   const InfoSuplex = "./img/Tela-Suplex-Info.png";
 
-  const [toggleTelaOne, setToggleTelaOne] = useState(true);
-  const [toggleTelaTwo, setToggleTelaTwo] = useState(true);
-
-  const changeImageOne = () => {
-    setToggleTelaOne(!toggleTelaOne);
-  }
-
-  const changeImageTwo = () => {
-    setToggleTelaTwo(!toggleTelaTwo);
-  }
-
   return (
     <div>
       <HomeCarousel />
@@ -34,22 +23,6 @@ export default function Home() {
           <CardHome picture={Shorts} link={"/shorts-calzas"} />
         </Row>
       </Container>
-      <div>
-        {toggleTelaOne ?
-          <Image src={TelaDry} alt="Imagen Tela Dry" fluid="true" style={{ width: "100%" }} onClick={changeImageOne} />
-          :
-          <Image src={InfoDry} alt="Info Tela Dry" fluid="true" style={{ width: "100%" }} onClick={changeImageOne} />
-        }
-      </div>
-      <div>
-        {toggleTelaTwo ?
-          <Image src={TelaSuplex} alt="Imagen Tela Suplex" fluid="true" style={{ width: "100%" }} onClick={changeImageTwo} />
-          :
-          <Image src={InfoSuplex} alt="Info Tela Suplex" fluid="true" style={{ width: "100%" }} onClick={changeImageTwo} />
-        }
-      </div>
-      <hr />
-
       <Carousel indicators={false} >
         <Carousel.Item>
           <img className="d-block w-100" src={TelaDry} alt="Imagen Tela Dry" />
@@ -66,11 +39,6 @@ export default function Home() {
           <img className="d-block w-100" src={InfoSuplex} alt="Info Tela Suplex" />
         </Carousel.Item>
       </Carousel>
-      
-      {/* Falta:
-        Reemplazar "Sobre Nosotros" cuando tengamos la imagen correcta.
-      */}
-
       <Image src={Nosotros} alt="Sobre Nosotros" fluid="true" style={{ width: "100%" }} />
       <Image src={Pagos} alt="Formas de Pago" fluid="true" style={{ width: "100%" }} />
     </div>
